afcf5b8e645da325cb60312ecfdc8a49501d2004
Luc <lucfbng@protonmail.com>
2025-01-14 10:46:53 +0100

    design skill section, start project section
    - Donne all svg for skill section
    - Start project section but its broken

--- NATIF/ressources/script.js
+++ NATIF/ressources/script.js
@@ -7,3 +7,33 @@ function hideSidebar() {
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = 'none'
 }
+
+document.querySelectorAll('#leftskill').forEach((e) => { 
+    e.addEventListener('mouseover', () => {
+    document.getElementsByClassName('skill-section')[0].style.backgroundImage = 'url(images/skelleton-color-left.svg)'
+    })
+}) 
+
+
+document.querySelectorAll('#leftskill').forEach((e) => { 
+    e.addEventListener('mouseout', () => {
+        document.getElementsByClassName('skill-section')[0].style.background = 'url(images/skelleton-no-color.svg)'
+        document.getElementsByClassName('skill-section')[0].style.backgroundSize = 'cover'
+        document.getElementsByClassName('skill-section')[0].style.backgroundRepeat = 'no-repeat' 
+    })
+})
+
+document.querySelectorAll('#rightskill').forEach((e) => { 
+    e.addEventListener('mouseover', () => {
+    document.getElementsByClassName('skill-section')[0].style.backgroundImage = 'url(images/skelleton-color-right.svg)'
+    })
+}) 
+
+
+document.querySelectorAll('#rightskill').forEach((e) => { 
+    e.addEventListener('mouseout', () => {
+        document.getElementsByClassName('skill-section')[0].style.background = 'url(images/skelleton-no-color.svg)'
+        document.getElementsByClassName('skill-section')[0].style.backgroundSize = 'cover'
+        document.getElementsByClassName('skill-section')[0].style.backgroundRepeat = 'no-repeat' 
+    })
+})
